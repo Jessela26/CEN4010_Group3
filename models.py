43bbe182c102843045ff6c5f03efb596a89d04fa
Luis Bauza <hllywluis2@me.com>
2020-01-27 21:50:19 -0500

    Initial commit

---
+++ models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Wishlist(models.Model):
+    title = models.CharField(max_length=75) #Title of BOOK
+    author = models.CharField(max_length=30) # Name of AUTHOR
+    #picture = 
