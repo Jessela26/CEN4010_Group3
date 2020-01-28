ca32fa129ee08350dd85ec9ca173c87500808490
MBarc <42979055+MBarc@users.noreply.github.com>
2020-01-27 21:44:50 -0500

    Delete models.py

--- GeekText/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Wishlist(models.Model):
-    title = models.CharField(max_length=75) #Title of BOOK
-    author = models.CharField(max_length=30) # Name of AUTHOR
-    #picture = 
