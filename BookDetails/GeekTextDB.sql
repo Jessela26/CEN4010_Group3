e5c5f6f9e7e98577317f18bfdba80475ef220299
Edward Basulto <48189137+Ebasu004@users.noreply.github.com>
2020-02-17 21:30:51 -0500

    Add files via upload

---
+++ BookDetails/GeekTextDB.sql
@@ -0,0 +1,85 @@
+DROP TABLE IF EXISTS GeekText;
+
+CREATE TABLE GeekText(
+ID int Primary Key,
+Title varchar(355),
+Author varchar(355),
+Author_Bio varchar(1024),
+Publisher varchar(355),
+Release_date varchar(355));
+
+
+
+Insert Into GeekText values(1, 'Python for Data Analysis', 
+'Wes Mckinney', 'I have been developing data analysis software, mostly for use in the Python programming language. 
+My primary objective has been improving user productivity, increasing performance and efficiency, and enhancing data interoperability.',
+'O,Reilly','10/05/2012');
+
+Insert Into GeekText values(2, 'Java Script 20 Lessons to Successful Web Development', 
+'Robin Nixon', 'Robert Nixon is a prolific author on programming and Web Development with his books having been translated into numerous
+foreign languages - frequently topping the charts.',
+'Mc Graw Hill','01/27/2015');
+
+Insert Into GeekText values(3, 'Python for Data Analysis', 
+'Wes Mckinney', 'I have been developing data analysis software, mostly for use in the Python programming language. 
+My primary objective has been improving user productivity, increasing performance and efficiency, and enhancing data interoperability.',
+'O,Reilly','10/05/2012');
+
+Insert Into GeekText values(4, "Java For Dummies, 7th Edition", "Barry Burd", "Barry Burd is a professor of Computer Science and Mathematics at Drew University. He’s the author of several books in the For Dummies series, including Java For Dummies and Android Application Development All-in-One For Dummies. 
+He’s written articles for InfoQ.com, AndroidAuthority.com and TheServerSide.com. When he was a teaching assistant at the University of Illinois with full responsibility for each of his courses, he appeared for five out of his eight semesters on the university-wide List of Teachers Rated Excellent by their Students.",
+"For Dummies", "03/05/2017");
+
+Insert Into GeekText values(5, "Python All-in-One For Dummies", "John Shovic", "Author, Entrepreneur, Educator. Setting up a new Computer Science Program with University of Idaho/North Idaho College in Coeur d'Alene. CTO of SwitchDoc Labs.",
+"For Dummies", "03/07/2019");
+
+Insert Into GeekText values(6, "Algorithms For Dummies", "John Paul Mueller", 
+"John Paul Mueller. John Mueller is a freelance author and technical editor. He has writing in his blood, having produced 51 books and over 200 articles to date. The topics of his writing range from networking to artificial intelligence and from database management to heads-down programming.",
+"For Dummies", "04/24/2017");
+
+Insert Into GeekText values(7, "Coding All-in-One For Dummies", "Nikhil Abraham", "Nikhil Abraham was Director of Business Development & Growth at Codecademy. In that role, he taught and trained thousands of beginning coders across a variety of professions. He helped refine Codecademy's online courses, which have introduced basic coding skills to millions of learners.",
+"For Dummies", "03/01/2017");
+
+Insert Into GeekText values(8, "Web Coding & Development All-in-One For Dummies", "Paul McFedries", "Paul McFedries is a Canadian author of more than 40 computer books that have sold 3 million copies worldwide. He has been programming since he was a teenager in the mid-1970s, and is the president of Logophilia Limited, a technical writing company.",
+"For Dummies", "03/15/2018"); 
+
+Insert Into GeekText values(9, "Beginning Programming", "Wallace Wang", "Wallace Wang is a former Windows enthusiast who took one look at Vista and realized that the future of computing belonged to the Mac. He s written more than 40 computer books, including Microsoft Office for Dummies, Beginning Programming for Dummies, Steal This Computer Book, My New Mac, and My New iPad. In addition to programming the Mac and iPhone/iPad, he also performs stand-up comedy, having appeared on A&E s 'Evening at the Improv,' and having performed in Las Vegas at the Riviera Comedy Club at the Riviera Hotel & Casino. When he’s not writing computer books or performing stand-up comedy, he also enjoys blogging about screenwriting at his site, The 15 Minute Movie Method, where he shares screenwriting tips with other aspiring screenwriters who all share the goal of breaking into Hollywood.",
+"For Dummies", "06/03/2008");
+
+Insert Into GeekText values(10, "R For Dummies", "Andrie de Vries", "Andrie de Vries is a leading R expert and Business Services Director for Revolution Analytics. With over 20 years of experience, he provides consulting and training services in the use of R. Joris Meys is a statistician, R programmer and R lecturer with the faculty of Bio-Engineering at the University of Ghent.",
+"For Dummies", "06/07/2015");
+
+Insert Into GeekText values(11,"C++ All-in-One For Dummies", "John Paul Mueller", "John Paul Mueller. John Mueller is a freelance author and technical editor. He has writing in his blood, having produced 51 books and over 200 articles to date. The topics of his writing range from networking to artificial intelligence and from database management to heads-down programming.",
+"For Dummies", "08/18/2014");
+
+Insert Into GeekText values(12, "Java All-in-One For Dummies", "Doug Lowe", "Doug Lowe is a master at simplifying complex tech topics. His four previous editions of Java All-in-One For Dummies have helped thousands of programmers master Java skills. Doug also serves as a one-man IT department for an engineering consulting firm.",
+"For Dummies", "05/01/2017");
+
+Insert Into GeekText values(13, "Java Programming for Android Developers For Dummies", "Barry Burd", "Barry Burd, PhD, is a professor in the Department of Mathematics and Computer Science at Drew University in Madison, New Jersey. He has lectured at conferences in the United States, Europe, Australia, and Asia. He hosts podcasts and videos about software and other technology topics. He is the author of many articles and books, including Java For Dummies.",
+"For Dummies", "11/07/2016");
+
+Insert Into GeekText values(14, "Android App Development FD 3e (For Dummies)", "Michael Burton", "Michael Burton is the Director of Mobile Engineering at Groupon and creator of the popular RoboGuice programming framework for Android. He's the author of the OpenTable, Digg, TripIt, and award winning Groupon Android apps, and has addressed conferences around the world.",
+"For Dummies", "05/09/2015");
+
+Insert Into GeekText values(15, "Networking All-in-One For Dummies", "Doug Lowe", "Doug Lowe is a master at simplifying complex tech topics. His four previous editions of Java All-in-One For Dummies have helped thousands of programmers master Java skills. Doug also serves as a one-man IT department for an engineering consulting firm.",
+"For Dummies", "04/10/2018");
+
+Insert Into GeekText values(16, "Cybersecurity For Dummies", "Joseph Steinberg", "Joseph Steinberg is a cybersecurity and emerging technologies advisor with two decades of industry experience. One of only 28 people worldwide to hold the entire suite of advanced information security certifications (CISSP, ISSAP, ISSMP, and CSSLP), he writes an independent column covering cybersecurity and privacy, after previously covering those topics for Forbes and Inc. Magazine. He also authors thought leadership articles for many technology companies, including IBM and Microsoft, and has invented various cybersecurity-related technologies, which are cited in more than 400 U.S. patent filings.",
+"For Dummies", "10/15/2019");
+
+Insert Into GeekText values(17, "Hacking For Dummies", "Kevin Beaver", "Kevin Beaver is an independent information security consultant with more than three decades of experience. Kevin specializes in performing vulnerability and penetration testing and security consulting work for Fortune 1000 corporations, product vendors, independent software developers, universities, and government organizations. He has appeared on CNN and been quoted in The Wall Street Journal.",
+"For Dummies", "07/11/2018");
+
+Insert Into GeekText values(18, "PHP, MySQL, & JavaScript All-in-One For Dummies", "Blum", "Richard Blum has more than 30 years of experience as a systems administrator and programmer. He teaches online courses in PHP, JavaScript, HTML5, and CSS3 programming, and authored the latest edition of Linux For Dummies.",
+"For Dummies", "04/10/2018");
+
+Insert Into GeekText values(19, "PHP & MySQL For Dummies", "Janet Valade", "Janet Valade is a technical writer, Web designer/programmer, and systems analyst. She has designed and developed data archives, supervised computer resource operations, and conducted seminars and workshops. She has written all previous editions of PHP & MySQL For Dummies.",
+"For Dummies", "12/09/2009"); 
+
+Insert Into GeekText values(20, "Python for Dummies", "Stef Maruch", "Stef Maruch got her hands on an original 128K Mac in 1984 and has been writing about computers ever since. She has over fifteen years’ experience in instructional design, writing, and editing end-user computer manuals, including tutorials and user’s guides for Apple Newton, HyperCard and HyperTalk, and DVD Studio Pro.",
+"For Dummies", "09/04/2006");
+
+Insert Into GeekText values(21, "Python for Data Science For Dummies", "John Paul Mueller", "John Paul Mueller is a tech editor and the author of over 100 books on topics from networking and home security to database management and heads-down programming. Follow John's blog at blog.johnmuellerbooks.com/. Luca Massaron is a data scientist who specializes in organizing and interpreting big data and transforming it into smart data. He is a Google Developer Expert (GDE) in machine learning.",
+"For Dummies", "02/27/2019");
+
+Insert Into GeekText values(22, "SQL All-In-One For Dummies", "Allen G. Taylor", "Allen G. Taylor is a 40-year veteran of the computer industry and the author of more than 40 books, including SQL For Dummies and Database Development For Dummies. For the latest news on Allen's activities, check out his online courses (at pioneer-academy1.teachable.com) and his blog (at www.allengtaylor.com). You can contact Allen at allen.taylor@ieee.org.",
+"For Dummies", "04/23/2019");
