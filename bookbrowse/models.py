e36d20e84bba3777a2059bb57eb12683b7d0d0a9
Luis Bauza <hllywluis2@me.com>
2020-02-17 22:01:00 -0500

    Added more fields for the Book model.

--- bookbrowse/models.py
+++ bookbrowse/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=75)
     author = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000, null=True)
+    year_published = models.DateField(max_length=50, null=True)
+    rating = models.IntegerField(null=True)
