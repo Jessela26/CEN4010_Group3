ab8624553023e0a6a9fa245598ea169aab1be614
Luis Bauza <hllywluis2@me.com>
2020-02-22 12:47:30 -0500

    Add publisher and author bio to the Book object.

--- Browse/models.py
+++ Browse/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=75)
     author = models.CharField(max_length=30)
+    author_bio = models.CharField(max_length=5000, null=True)
+    publisher = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=1000, null=True)
     year_published = models.DateField(max_length=50, null=True)
     rating = models.IntegerField(null=True)
--- GeekText/urls.py
+++ GeekText/urls.py
@@ -13,21 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 from django.views.generic.base import RedirectView
+
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls, name='admin'),
-    url(r'^home/', views.homepage, name='home'),
-    url(r'^wishlist/$', views.wishlist, name='wishlist'),
-    url(r'^browse/', include('Browse.urls'), name='browse'),
-    url(r'^cart/$', views.cart, name='cart'),
-    url(r'^$', RedirectView.as_view(url='home', permanent=True), name='index'),
-    path('browse/details/<int:book_id>/', include('BookDetails.urls'), name='details')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+                  url(r'^admin/', admin.site.urls, name='admin'),
+                  url(r'^home/', views.homepage, name='home'),
+                  url(r'^wishlist/$', views.wishlist, name='wishlist'),
+                  url(r'^browse/', include('Browse.urls'), name='browse'),
+                  url(r'^cart/$', views.cart, name='cart'),
+                  url(r'^$', RedirectView.as_view(url='home', permanent=True), name='index'),
+                  path('browse/details/<int:book_id>/', include('BookDetails.urls'), name='details')
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
