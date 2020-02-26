a9884f052e283325048fe2d920df2dec3ee0d69c
Luis Bauza <hllywluis2@me.com>
2020-02-26 18:20:18 -0500

    Add description to browse.html

--- Browse/views.py
+++ Browse/views.py
@@ -6,5 +6,12 @@ from Browse.models import Book
 def browse(request):
     args = {}
     books = Book.objects.all()
+    for book in books:
+        if book.description.__contains__('.'):
+            book.description = book.description.split('.', 1)[0] + '.'
+        if book.description.__contains__('!'):
+            book.description = book.description.split('!', 1)[0] + '!'
+        if book.description.__contains__('?'):
+            book.description = book.description.split('?', 1)[0] + '?'
     args['books'] = books
     return render(request, "browse.html", args)
