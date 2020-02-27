b7a7a5a954641ffac9656c9ff1e7b8c2566d745d
Luis Bauza <hllywluis2@me.com>
2020-02-27 02:26:43 -0500

    Added pagination and a bottom bar for nav.html. Browse is now sorted alphabetically by default.

--- Browse/views.py
+++ Browse/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from Browse.models import Book
 
 
 # Create your views here.
 def browse(request):
     args = {}
-    books = Book.objects.all()
+    books = Book.objects.order_by('title')
+    paginator = Paginator(books, 10)
     for book in books:
         if book.description.__contains__('.'):
             book.description = book.description.split('.', 1)[0] + '.'
@@ -13,5 +15,7 @@ def browse(request):
             book.description = book.description.split('!', 1)[0] + '!'
         if book.description.__contains__('?'):
             book.description = book.description.split('?', 1)[0] + '?'
-    args['books'] = books
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    args['books'] = page_obj
     return render(request, "browse.html", args)
