e97a6bfa8e53b8f44343725deb52a3610224a1cb
Luis Bauza <hllywluis2@me.com>
2020-03-16 21:28:44 -0400

    Fix some inconsistencies.

--- Browse/urls.py
+++ Browse/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('', views.browse, name='browse'),
-    path('sort/by_author', views.author, name='by_author'),
-    path('sort/recent', views.p_date_new2old, name='p_date_new2old'),
-    path('sort/older', views.p_date_old2new, name='p_date_old2new'),
+    path('sort/by_author/', views.author, name='by_author'),
+    path('sort/recent/', views.p_date_new2old, name='p_date_new2old'),
+    path('sort/older/', views.p_date_old2new, name='p_date_old2new'),
     url('search/$', views.browse, name='search')
 ]
