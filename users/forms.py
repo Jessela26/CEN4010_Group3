203a4ee0b14d56125c672200a4914740d9a20fb5
dannybarlos <47197220+dannybarlos@users.noreply.github.com>
2020-03-28 19:44:28 -0400

    Created Shipping Info Form

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from users.models import Payment
+from users.models import Payment, Address
 from django.contrib.auth.forms import UserCreationForm
 from creditcards.forms import CardNumberField, CardExpiryField, SecurityCodeField
 
@@ -33,5 +33,17 @@ class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
         fields = ['cc_number', 'cc_expiry','cc_code']
+
+class AddressForm(forms.ModelForm):
+    address_1 = forms.CharField(max_length=30)
+    address_2 = forms.CharField(max_length=50, required = False)
+    city = forms.CharField(max_length=60)
+    state = forms.CharField(max_length=30)
+    zip_code = forms.CharField(max_length=5)
+    country = forms.CharField(max_length=50)
+
+    class Meta:
+        model = Address
+        fields = ['address_1','address_2','city','state','zip_code', 'country']
 
 
