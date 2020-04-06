accba0339e6cd29ea98318bcb3fcfa73f2e46e46
dannybarlos <47197220+dannybarlos@users.noreply.github.com>
2020-04-05 21:17:01 -0400

    Updated Payment and Address Models

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from creditcards.models import CardNumberField, CardExpiryField, SecurityCodeField
 
+from django.core.validators import MinLengthValidator
+
+
 
 # Create your models here.
 class Profile(models.Model):#associating User model with Profile model
@@ -10,16 +13,23 @@ class Profile(models.Model):#associating User model with Profile model
         return f'{self.user.username} Profile'
 
 
-
 class Payment(models.Model):
-    cc_number = CardNumberField('card number')
-    cc_expiry = CardExpiryField('expiration date')
-    cc_code = SecurityCodeField('security code')
+    user = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
+    #cc_number = CardNumberField('card number')
+    #cc_expiry = CardExpiryField('expiration date')
+    #cc_code = SecurityCodeField('security code')
+    cc_number = models.CharField(max_length = 20, validators=[MinLengthValidator(15)])
+    cc_expiry = models.DateField(help_text = "Please use the following format: <em>MM/YY</em>.")
+    cc_code = models.CharField(max_length = 3, help_text = "Please use the following format: <em>CVV or CVC</em>.", validators=[MinLengthValidator(3)])
+
 
     def __str__(self):
-        return f'{self.user.username} Payment'
+        return f'{self.cc_number}' 
+
+ 
 
 class Address(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete= models.SET_NULL)
     address_1 = models.CharField(max_length=30)
     address_2 = models.CharField(max_length=50, blank = True)
     city = models.CharField(max_length=60, default="Miami")
