bf851e0ce1e8d224f22267c83314648997557fe0
Luis Bauza <hllywluis2@me.com>
2020-04-08 19:25:21 -0400

    Lots of little QoL fixes and changes

--- Browse/urls.py
+++ Browse/urls.py
@@ -5,9 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.browse, name='home'),
-    path('', views.browse, name='browse'),
-    path('sort/by_author/', views.author, name='by_author'),
-    path('sort/recent/', views.p_date_new2old, name='p_date_new2old'),
-    path('sort/older/', views.p_date_old2new, name='p_date_old2new'),
-    url('search/$', views.browse, name='search')
+    path('by_title', views.browse, name='browse'),
+    path('by_title&view_18', views.browse18, name='browse18'),
+    path('sort/by_author', views.author, name='by_author'),
+    path('sort/by_author&view_18', views.author18, name='by_author18'),
+    path('sort/recent', views.p_date_new2old, name='p_date_new2old'),
+    path('sort/recent&view_18', views.p_date_new2old18, name='p_date_new2old18'),
+    path('sort/older', views.p_date_old2new, name='p_date_old2new'),
+    path('sort/older&view_18', views.p_date_old2new18, name='p_date_old2new18'),
+    path('search/', views.browse, name='search')
 ]
--- Browse/views.py
+++ Browse/views.py
@@ -24,7 +24,7 @@ def browse(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     args['books'] = page_obj
-    args['url_name'] = 'title'
+    args['url_name'] = resolve(request.path_info).url_name
     return render(request, "browse.html", args)
 
 
@@ -104,6 +104,7 @@ def search(request, search_term):
     print(resolve(request.path_info).url_name)
     return render(request, "browse.html", args)
 
+
 def browse18(request):
     if request.method == 'POST':
         return search(request, request.POST)
@@ -120,5 +121,59 @@ def browse18(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     args['books'] = page_obj
-    args['url_name'] = 'title'
+    args['url_name'] = resolve(request.path_info).url_name
+    return render(request, "browse.html", args)
+
+
+def author18(request):
+    args = {}
+    books = Book.objects.order_by('author')
+    paginator = Paginator(books, 18)
+    for book in books:
+        if book.description.__contains__('.'):
+            book.description = book.description.split('.', 1)[0] + '.'
+        if book.description.__contains__('!'):
+            book.description = book.description.split('!', 1)[0] + '!'
+        if book.description.__contains__('?'):
+            book.description = book.description.split('?', 1)[0] + '?'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    args['books'] = page_obj
+    args['url_name'] = resolve(request.path_info).url_name
+    return render(request, "browse.html", args)
+
+
+def p_date_old2new18(request):
+    args = {}
+    books = Book.objects.order_by('year', 'date_published')
+    paginator = Paginator(books, 18)
+    for book in books:
+        if book.description.__contains__('.'):
+            book.description = book.description.split('.', 1)[0] + '.'
+        if book.description.__contains__('!'):
+            book.description = book.description.split('!', 1)[0] + '!'
+        if book.description.__contains__('?'):
+            book.description = book.description.split('?', 1)[0] + '?'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    args['books'] = page_obj
+    args['url_name'] = resolve(request.path_info).url_name
+    return render(request, "browse.html", args)
+
+
+def p_date_new2old18(request):
+    args = {}
+    books = Book.objects.order_by('-year', '-date_published')
+    paginator = Paginator(books, 18)
+    for book in books:
+        if book.description.__contains__('.'):
+            book.description = book.description.split('.', 1)[0] + '.'
+        if book.description.__contains__('!'):
+            book.description = book.description.split('!', 1)[0] + '!'
+        if book.description.__contains__('?'):
+            book.description = book.description.split('?', 1)[0] + '?'
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    args['books'] = page_obj
+    args['url_name'] = resolve(request.path_info).url_name
     return render(request, "browse.html", args)
