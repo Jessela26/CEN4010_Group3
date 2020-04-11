dd83d5f249e2d1c09043ce4ee229790c8917a946
Luis Bauza <hllywluis2@me.com>
2020-04-10 23:13:55 -0400

    Added price sorting

--- Browse/urls.py
+++ Browse/urls.py
@@ -5,13 +5,17 @@ from . import views
 
 urlpatterns = [
     path('', views.browse, name='home'),
-    path('by_title', views.browse, name='browse'),
-    path('by_title&view_18', views.browse18, name='browse18'),
+    path('sort/by_title', views.browse, name='browse'),
+    path('sort/by_title&view_18', views.browse18, name='browse18'),
     path('sort/by_author', views.author, name='by_author'),
     path('sort/by_author&view_18', views.author18, name='by_author18'),
     path('sort/recent', views.p_date_new2old, name='p_date_new2old'),
     path('sort/recent&view_18', views.p_date_new2old18, name='p_date_new2old18'),
     path('sort/older', views.p_date_old2new, name='p_date_old2new'),
     path('sort/older&view_18', views.p_date_old2new18, name='p_date_old2new18'),
+    path('sort/price_lth', views.price_low_to_high, name='p_lth'),
+    path('sort/price_lth18', views.price_low_to_high18, name='p_lth18'),
+    path('sort/price_htl', views.price_high_to_low, name='p_htl'),
+    path('sort/price_htl18', views.price_high_to_low18, name='p_htl18'),
     path('search/', views.browse, name='search')
 ]
