c0af3a4b843a91fa3140215c3d0ac78ce1901fbd
Edward Basulto <48189137+Ebasu004@users.noreply.github.com>
2020-04-11 20:49:01 -0400

    Add files via upload

--- users/signals.py
+++ users/signals.py
@@ -1,16 +1,13 @@
-from django.contrib.auth.models import User  # sender
 from django.db.models.signals import post_save
-from django.dispatch import receiver  # receiver
+from django.contrib.auth.models import User#sender
+from django.dispatch import receiver#receiver
+from .models import Profile, Payment, Address
 
-from .models import Profile
-
-
-@receiver(post_save, sender=User)
+@receiver(post_save, sender = User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-
-@receiver(post_save, sender=User)
+@receiver(post_save, sender = User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
